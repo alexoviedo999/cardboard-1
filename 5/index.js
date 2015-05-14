77fce20641c0258c332b3a299ff06f93cee295e6
alexoviedo <alexoviedo999@gmail.com>
2015-05-13 21:53:50 -0400

    Redue on space sketch.

--- 5/index.js
+++ 5/index.js
@@ -4,7 +4,7 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
       var camera, scene, renderer, effect, element;
 
-      var mesh, lightMesh, geometry;
+      var mesh, lightMesh, geometry, pivotPoint, cube;
       var spheres = [];
       var cubes = [];
 
@@ -30,17 +30,10 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
         stereoStatus = true;
 
         camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 100000 );
-        camera.position.z = 3200;
+        camera.position.z = 100;
+        camera.position.y = 500;
 
         scene = new THREE.Scene();
-
-        // var path = "textures/cube/skybox/";
-        var format = '.png';
-        // var urls = [
-        //   path + 'px' + format, path + 'nx' + format,
-        //   path + 'py' + format, path + 'ny' + format,
-        //   path + 'pz' + format, path + 'nz' + format
-        // ];
 
         var urls = [
           '../images/small-stars/right.png',
@@ -51,60 +44,95 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
           '../images/small-stars/back.png'
         ];
 
+        
+        // Cubes
+        var geometry = new THREE.BoxGeometry( 1500, 1500, 1500, 1, 1, 1 );
+        var textureCube = THREE.ImageUtils.loadTextureCube( urls, THREE.CubeRefractionMapping );
+        var material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube, refractionRatio: 0.99 } );
+
+        // for ( var i = 0; i < 200; i ++ ) {
+          cube = new THREE.Mesh( geometry, material );
+
+          cube.position.x = 5800;
+          cube.position.y = -1000;
+          // cube.position.z = 0;
+          // cube.rotation.x = 45;
+          // cube.rotation.y = 45;
+          // cube.rotation.z = 45;
+
+          // cube.position.x = Math.random() * 10000 - 5000;
+          // cube.position.y = Math.random() * 10000 - 5000;
+          // cube.position.z = Math.random() * 10000 - 5000;
+          // cube.scale.x = cube.scale.y = cube.scale.z = Math.random() * 3 + 1;
+
+          // cube.scale.x = cube.scale.y = cube.scale.z = 600;
+          cube.name = 'cube';
+          scene.add( cube );
+          // cubes.push( cube );
+        // }
+
+        // add an object as pivot point to the sphere 
+          pivotPoint = new THREE.Object3D(); 
+          cube.add(pivotPoint);
+
+       
+
 
         // Spheres
         var geometry = new THREE.SphereGeometry( 100, 32, 16 );
-        var textureCube = THREE.ImageUtils.loadTextureCube( urls, THREE.CubeRefractionMapping );
-        var material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube, refractionRatio: 0.95 } );
 
-        for ( var i = 0; i < 300; i ++ ) {
+        // var material = new THREE.MeshLambertMaterial();
+        // var material = new THREE.Mesh(sphere, spherMat);
+
+        var textureCube = THREE.ImageUtils.loadTextureCube( urls, THREE.CubeRefractionMapping );
+        var material = new THREE.MeshBasicMaterial( { color: "#d7dbd0", envMap: textureCube, refractionRatio: 0.99 } );
+        
+        var datGUI  = new dat.GUI()
+
+          
+
+        for ( var i = 0; i < 20; i ++ ) {
 
           var mesh = new THREE.Mesh( geometry, material );
-          mesh.position.x = Math.random() * 10000 - 5000;
-          mesh.position.y = Math.random() * 10000 - 5000;
-          mesh.position.z = Math.random() * 10000 - 5000;
+          glowMesh  = new THREEx.GeometricGlowMesh(mesh);
+          // mesh.position.x = Math.random() * 4500 - 3500;
+          // mesh.position.y = Math.random() * 700 - 500;
+          // mesh.position.z = 4000;
+          var rndm = Math.random() * 12500 - 6500;
+
+          if(rndm > 1000 || rndm < -1000) {
+            mesh.position.x = rndm
+          }
+          else {
+            rndm = Math.random() * 1000 + 10000;
+            mesh.position.x = rndm
+          }
+      
           mesh.scale.x = mesh.scale.y = mesh.scale.z = Math.random() * 3 + 1;
-          scene.add( mesh );
+          
+          mesh.add(glowMesh.object3d)
+
+
+
+        //         // SUPER SIMPLE GLOW EFFECT
+        // // use sprite because it appears the same from all angles
+        // var spriteMaterial = new THREE.SpriteMaterial( 
+        // { 
+        //   map: new THREE.ImageUtils.loadTexture( '../images/glow.png' ), 
+          
+        //   color: 0x0000ff, transparent: false, blending: THREE.AdditiveBlending
+        // });
+        // var sprite = new THREE.Sprite( spriteMaterial );
+        // sprite.scale.set(200, 200, 1.0);
+        // mesh.add(sprite); // this centers the glow at the mesh
+
+
+          pivotPoint.add( mesh );
           spheres.push( mesh );
         }
 
-
-
-        // // Point Cloud
-        // var sphereGeometry = new THREE.SphereGeometry( 1000, 32, 16 );
-        // var pointMat = new THREE.PointCloudMaterial(sphereGeometry);
-        // pointMat.map = THREE.ImageUtils.loadTexture("../textures/ps_smoke.png");
-        // pointMat.color = new THREE.Color(0x5555ff);
-        // pointMat.transparent = true;
-        // pointMat.size = 2.2;
-        // pointMat.blending = THREE.AdditiveBlending;
-
-        // var pointCloud = new THREE.PointCloud(sphereGeometry, pointMat);
-        // pointCloud.position.x = 2000;
-        // pointCloud.position.y = 2000;
-        // pointCloud.position.z = 2000;
-        // pointCloud.sizeAttenuation = true;
-        // pointCloud.sortPoints = true;
-        // scene.add(pointCloud);
-
-
-
-        // Cubes
-        var geometry = new THREE.BoxGeometry( 200, 200, 200, 1, 1, 1 );
-        var textureCube = THREE.ImageUtils.loadTextureCube( urls, THREE.CubeRefractionMapping );
-        var material = new THREE.MeshBasicMaterial( { color: 0xffffff, envMap: textureCube, refractionRatio: 0.95 } );
-
-        for ( var i = 0; i < 200; i ++ ) {
-          var cube = new THREE.Mesh( geometry, material );
-          cube.position.x = Math.random() * 10000 - 5000;
-          cube.position.y = Math.random() * 10000 - 5000;
-          cube.position.z = Math.random() * 10000 - 5000;
-          cube.scale.x = cube.scale.y = cube.scale.z = Math.random() * 3 + 1;
-          cube.name = 'cube';
-          scene.add( cube );
-          cubes.push( cube );
-        }
-
+        new THREEx.addAtmosphereMaterial2DatGui(glowMesh.insideMesh.material, datGUI)
+          
 
         // Skybox
 
@@ -143,15 +171,24 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
         //variable control object
         varControl = new function() { 
-          this.rotationSpeed = 0.010; 
+          this.rotationSpeed = 0.010;
+          this.sphereGlowPower = 1.4;
+          this.sphereOrbitSpeedX = 0.00001;
+          this.rotationSpeedX = 0.00001;
+          this.rotationSpeedY = 0.00001;
+          this.rotationSpeedZ = 0.00001;
+          this.rotationX = 0.00001;
+          this.rotationY = 0.00001;
+          this.rotationZ = 0.00001;
+
           this.stereoToggle = function() {
-            stereoStatus = !stereoStatus;
-        windowHalfX = window.innerWidth,
-        windowHalfY = window.innerHeight,
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-        effect.setSize( window.innerWidth, window.innerHeight );
-          }
+          stereoStatus = !stereoStatus;
+          windowHalfX = window.innerWidth,
+          windowHalfY = window.innerHeight,
+          camera.aspect = window.innerWidth / window.innerHeight;
+          camera.updateProjectionMatrix();
+          effect.setSize( window.innerWidth, window.innerHeight );
+        }
           // this.scale = 1; 
         }; 
 
@@ -161,8 +198,22 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
           var gui = new dat.GUI();
           gui.add(controlObject, 'rotationSpeed', -0.1, 0.1);
           gui.add(controlObject, 'stereoToggle').name('VR Mode');
-          // gui.add(controlObject, 'scale', 0.01, 2);
-        }
+          gui.add(controlObject, 'sphereGlowPower', 0, 3);
+          gui.add(controlObject, 'sphereOrbitSpeedX', -0.2, 0.2);
+          gui.add(controlObject, 'rotationSpeedX', -0.2, 0.2);
+          gui.add(controlObject, 'rotationSpeedY', -0.2, 0.2);
+          gui.add(controlObject, 'rotationSpeedZ', -0.2, 0.2);
+          gui.add(controlObject, 'rotationX').onChange(function (v) {
+              pivotPoint.rotation.x = v
+          });
+          gui.add(controlObject, 'rotationY').onChange(function (v) {
+              pivotPoint.rotation.y = v
+          });
+          gui.add(controlObject, 'rotationZ').onChange(function (v) {
+              pivotPoint.rotation.z = v
+          });
+            // gui.add(controlObject, 'scale', 0.01, 2);
+          }
 
         window.addEventListener('deviceorientation', setOrientationControls, true);
         window.addEventListener( 'resize', onWindowResize, false );
@@ -206,20 +257,37 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
       function animate() {
         var timer = 0.0001 * Date.now();
+        pivotPoint.rotation.x += varControl.sphereOrbitSpeedX;
+        pivotPoint.rotation.x += varControl.rotationSpeedX;
+        pivotPoint.rotation.y += varControl.rotationSpeedY;
+        pivotPoint.rotation.z += varControl.rotationSpeedZ;
+        pivotPoint.rotationX = varControl.rotationX;
+        pivotPoint.rotationY = varControl.rotationY;
+        pivotPoint.rotationZ = varControl.rotationZ;
 
         for ( var i = 0, il = spheres.length; i < il; i ++ ) {
           var sphere = spheres[ i ];
-          sphere.position.x = 5000 * Math.cos( timer + i );
-          sphere.position.y = 5000 * Math.sin( timer + i * 1.1 );
+          // sphere.position.x = 5000 * Math.cos( timer + i );
+          // sphere.position.y = 5000 * Math.sin( timer + i * 1.1 );
+          sphere.position.z = 5000 * Math.sin( timer + i * 1.1 );
+
+          // Math.random() * 12500 - 6500
+
+          
+
+          
+          glowMesh.insideMesh.material.uniforms.power.value = varControl.sphereGlowPower;
         }
 
-        for ( var i = 0, il = cubes.length; i < il; i ++ ) {
-          var cube = cubes[ i ];
-          cube.position.x = 5000 * Math.cos( timer + i );
-          cube.position.y = 5000 * Math.sin( timer + i * 1.1 );
-          cube.rotation.y += varControl.rotationSpeed;
-          // cube.scale.set(varControl.scale, varControl.scale, varControl.scale);
-        }
+        cube.rotation.y += varControl.rotationSpeed;
+
+        // for ( var i = 0, il = cubes.length; i < il; i ++ ) {
+        //   var cube = cubes[ i ];
+        //   cube.position.x = 5000 * Math.cos( timer + i );
+        //   cube.position.y = 5000 * Math.sin( timer + i * 1.1 );
+        //   cube.rotation.y += varControl.rotationSpeed;
+        //   // cube.scale.set(varControl.scale, varControl.scale, varControl.scale);
+        // }
 
         camera.updateProjectionMatrix();
         requestAnimationFrame( animate );
@@ -227,8 +295,10 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
       }
 
       function render() {
+
         if(stereoStatus){
           effect.render( scene, camera );
+
         }
         else {
           renderer.render( scene, camera );
